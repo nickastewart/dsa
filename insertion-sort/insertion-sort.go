3809dcf1597168d23e478fd94df68d8319fcb5ed
nickstewart <nick.stewart1993@gmail.com>
2025-06-30 22:56:52 +0100

    insertion sort descending

--- insertion-sort/insertion-sort.go
+++ insertion-sort/insertion-sort.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 	arr := []int{31, 41, 59, 26, 41, 58}
 	fmt.Println(insertionSortAscending(arr))
+	fmt.Println(insertionSortDescending(arr))
 }
 
 func insertionSortAscending(arr []int) []int {
@@ -12,6 +13,19 @@ func insertionSortAscending(arr []int) []int {
 		key := arr[i]
 		var j int = i - 1
 		for j > -1 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j = j - 1
+		}
+		arr[j+1] = key
+	}
+	return arr
+}
+
+func insertionSortDescending(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		var j int = i - 1
+		for j > -1 && arr[j] < key {
 			arr[j+1] = arr[j]
 			j = j - 1
 		}
